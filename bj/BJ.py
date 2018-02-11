9af9d8e848563151128e99f8aa2b9f04b5205033
Greg Donald <gdonald@gmail.com>
2018-02-11 12:14:59 -0600

    card faces, regular shoe

--- bj/BJ.py
+++ bj/BJ.py
@@ -1,20 +1,4 @@
 
-class Card:
-
-    def __init__(self, value, suite):
-        self.value = value
-        self.suite = suite
-
-    def __str__(self):
-        return u'%s %s' % (self.suite, self.value)
-
-
-class Shoe:
-
-    def __init__(self):
-        pass
-
-
 class Hand:
 
     def __init__(self):
@@ -38,3 +22,78 @@ class Game():
     def __init__(self):
         pass
 
+
+class Shoe:
+
+    shuffle_specs = [[95, 8],
+		     [92, 7],
+		     [89, 6],
+		     [86, 5],
+		     [84, 4],
+		     [82, 3],
+		     [81, 2],
+		     [80, 1]]
+
+    def __init__(self, num_decks):
+        self.num_decks = num_decks
+        self.cards = []
+
+    def need_to_shuffle(self):
+        if len(self.cards) == 0:
+            return True
+
+        total_cards = self.num_decks * 52
+        cards_dealt = total_cards - len(self.cards)
+        used = (cards_dealt / total_cards) * 100.0
+
+        for x in range(len(Shoe.shuffle_specs)):
+            if used > Shoe.shuffle_specs[x][0] and self.num_decks == Shoe.shuffle_specs[x][1]:
+                return True
+
+        return False
+
+    def shuffle(self):
+        from random import shuffle as sh
+        self.new_regular()
+        sh(self.cards)
+
+    def get_next_card(self):
+        return self.cards.pop(0)
+
+    def new_regular(self):
+        self.cards = []
+        for decks in range(self.num_decks):
+            for suite in range(4):
+                for value in range(13):
+                    self.cards.append(Card(value, suite))
+
+
+class Card:
+
+    faces = [["🂡", "🂱", "🃁", "🃑"],
+	     ["🂢", "🂲", "🃂", "🃒"],
+	     ["🂣", "🂳", "🃃", "🃓"],
+	     ["🂤", "🂴", "🃄", "🃔"],
+	     ["🂥", "🂵", "🃅", "🃕"],
+	     ["🂦", "🂶", "🃆", "🃖"],
+	     ["🂧", "🂷", "🃇", "🃗"],
+	     ["🂨", "🂸", "🃈", "🃘"],
+	     ["🂩", "🂹", "🃉", "🃙"],
+	     ["🂪", "🂺", "🃊", "🃚"],
+	     ["🂫", "🂻", "🃋", "🃛"],
+	     ["🂭", "🂽", "🃍", "🃝"],
+	     ["🂮", "🂾", "🃎", "🃞"],
+	     ["🂠", "",  "",  "" ]]
+
+    def __init__(self, value, suite):
+        self.value = value
+        self.suite = suite
+
+    def __str__(self):
+        return u'%s' % Card.faces[self.value][self.suite]
+
+    def is_ace(self):
+        return self.value == 0
+
+    def is_ten(self):
+        return self.value > 8
--- blackjack.py
+++ blackjack.py
@@ -5,5 +5,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'bj'))
 
 from BJ import *
 
-c = Card(0, 0)
+s = Shoe(8)
+
+if s.need_to_shuffle():
+    s.shuffle()
+
+c = s.get_next_card()
+print(c)
+
+c = s.get_next_card()
 print(c)
